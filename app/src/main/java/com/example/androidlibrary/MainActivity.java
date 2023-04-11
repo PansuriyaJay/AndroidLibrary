4fb0ddfcb4ce7b6f9b808f3fcf607082c1bc9c36
Siddharth-101 <siddharthsavaliya1@gmail.com>
2023-04-11 15:28:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/androidlibrary/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.androidlibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tosty.Toaster;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toaster.simpleToast(MainActivity.this,"This is my own library");
+
+    }
+}
